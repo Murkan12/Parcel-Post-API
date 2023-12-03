4a7fb224a0068cd4b872abcaee080fc2975f6598
murkan <loud3ds@gmail.com>
2023-12-03 13:08:50 +0100

    Initial commit

---
+++ src/main/java/com/parcel/parcelpostapi/ParcelPostApiApplication.java
@@ -0,0 +1,13 @@
+package com.parcel.parcelpostapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcelPostApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParcelPostApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parcel/parcelpostapi/ParcelPostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcel.parcelpostapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelPostApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
